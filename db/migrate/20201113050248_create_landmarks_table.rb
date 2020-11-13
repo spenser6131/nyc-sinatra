bce35032afac2e3832d3e5f4bf36f7da8c52785b
spenser6131 <brinkman.spenser@gmail.com>
2020-11-13 15:19:16 -0800

    Done.

---
+++ db/migrate/20201113050248_create_landmarks_table.rb
@@ -0,0 +1,9 @@
+class CreateLandmarksTable < ActiveRecord::Migration
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
+  end
+end
